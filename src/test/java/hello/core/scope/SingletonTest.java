85ca599a8719ccccfb49d5b6a53fe86066e7a5d9
hajune <buea486@gmail.com>
2023-11-06 14:53:54 +0900

    싱글톤 스코프

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,40 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singleton1 = ac.getBean(SingletonBean.class);
+        SingletonBean singleton2 = ac.getBean(SingletonBean.class);
+        System.out.println("singleton1 = " + singleton1);
+        System.out.println("singleton2 = " + singleton2);
+        assertThat(singleton1).isSameAs(singleton2);
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        void init() {
+            System.out.println("SingletonBean.init");
+
+        }
+
+        @PreDestroy
+        void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+
+    }
+}
